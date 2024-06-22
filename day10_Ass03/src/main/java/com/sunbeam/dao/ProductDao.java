6a1e464b3527bb0cf750e2a6b0f8d8bb04701c68
KD4-Balkisan-83600 <162438144+KD4-Balkisan-83600@users.noreply.github.com>
2024-06-23 00:26:49 +0530

    Day10_Ass03 Uploaded

---
+++ day10_Ass03/src/main/java/com/sunbeam/dao/ProductDao.java
@@ -0,0 +1,18 @@
+package com.sunbeam.dao;
+
+import java.util.List;
+
+import com.app.entities.Product;
+
+public interface ProductDao {
+//add a product
+	String addProduct(Long categoryId, Product newProduct);
+
+	// get all products
+	List<Product> getAllProducts();
+
+	// get product details by id
+	Product getProductDetails(Long productId);
+
+	String purchaseProduct(Long productId, int qty);
+}
